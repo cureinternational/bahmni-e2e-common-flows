5a0f79c3fd2a68f86357ee9c04c7f9a80866f498
gokultw <gokul.kuppan@thoughtworks.com>
2023-03-18 21:16:40 +0530

    Fixed Observation forms failures

--- tests/util/taikoHelper.js
+++ tests/util/taikoHelper.js
@@ -100,17 +100,17 @@ async function validateFormFromFile(configurations) {
     for (var configuration of configurations) {
         var label = configuration.label
         if (configuration.short_name !== undefined)
-            label = configuration.short_name
+            label = configuration.short_name.trim();
         switch (configuration.type) {
             case 'Group':
                 await validateFormFromFile(configuration.value)
                 break;
             case 'Date':
                 var dateFormatted = date.addDaysAndReturnDateInShortFormat(configuration.value)
-                assert.ok(await text(dateFormatted, toRightOf(label)).exists(), dateFormatted + " To Right of " + label + " is not exist.")
+                assert.ok(await $(`//LABEL[contains(normalize-space(), "${label}")]/../following-sibling::SPAN/PRE[normalize-space() = "${dateFormatted}"]`).exists(), dateFormatted + " To Right of " + label + " is not exist.")
                 break;
             default:
-                assert.ok(await text(configuration.value, toRightOf(label)).exists(), configuration.value + " To Right of " + label + " is not exist.")
+                assert.ok(await $(`//LABEL[contains(normalize-space(), "${label}")]/../following-sibling::SPAN/PRE[normalize-space() = "${configuration.value}"]`).exists(), configuration.value + " To Right of " + label + " is not exist.")
         }
     }
 }
